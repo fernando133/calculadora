19b95fc1876a400c8579e56885dd7d0da3fb7206
Fernando Martins <fernando@MacBook-Pro-de-Fernando.local>
2018-01-03 19:36:56 -0300

    adicionando CalculadoraService

--- src/app/calculadora/calculadora.module.ts
+++ src/app/calculadora/calculadora.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculadoraComponent } from './components';
+import { CalculadoraService } from './services';
 
 @NgModule({
   imports: [
@@ -9,6 +10,9 @@ import { CalculadoraComponent } from './components';
   declarations: [CalculadoraComponent],
   exports: [
     CalculadoraComponent
+  ],
+  providers: [
+    CalculadoraService
   ]
 })
 export class CalculadoraModule { }
