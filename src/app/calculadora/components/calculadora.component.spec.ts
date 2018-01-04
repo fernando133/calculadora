6c51a9fd02c242e5aa7ad63c9795e90742d25ead
Fernando Martins <fernando@MacBook-Pro-de-Fernando.local>
2018-01-04 10:39:37 -0300

    adicionando testes para CalculadoraService

--- src/app/calculadora/components/calculadora.component.spec.ts
+++ src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculadoraComponent } from './calculadora.component';
+import { CalculadoraService } from '../services';
 
 describe('CalculadoraComponent', () => {
   let component: CalculadoraComponent;
@@ -8,7 +8,10 @@ describe('CalculadoraComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CalculadoraComponent ]
+      declarations: [ CalculadoraComponent ],
+      providers: [
+        CalculadoraService
+      ]
     })
     .compileComponents();
   }));
--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -9,7 +9,43 @@ describe('CalculadoraService', () => {
     });
   });
 
-  it('should be created', inject([CalculadoraService], (service: CalculadoraService) => {
+  it('should be created', inject([CalculadoraService], 
+    (service: CalculadoraService) => {
     expect(service).toBeTruthy();
   }));
+
+  it ('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService)=>{
+      let soma = service.calcular(1,4, CalculadoraService.SOMA);
+      expect(soma).toEqual(5);
+    })
+  );
+
+  it ('deve garantir que 1 - 4 = -3',
+    inject([CalculadoraService], (service: CalculadoraService)=>{
+      let subtracao = service.calcular(1,4, CalculadoraService.SUBTRACAO);
+      expect(subtracao).toEqual(-3);
+    })
+  );
+
+  it ('deve garantir que 1 / 2 = 0.5',
+    inject([CalculadoraService], (service: CalculadoraService)=>{
+      let divisao = service.calcular(1,2, CalculadoraService.DIVISAO);
+      expect(divisao).toEqual(0.5);
+    })
+  );
+
+  it ('deve garantir que 2 * 5 = 10',
+    inject([CalculadoraService], (service: CalculadoraService)=>{
+      let multiplicacao = service.calcular(2,5, CalculadoraService.MULTIPLICACAO);
+      expect(multiplicacao).toEqual(10);
+    })
+  );
+
+  it ('deve retornar 0 para operacao invalida',
+    inject([CalculadoraService], (service: CalculadoraService)=>{
+      let invalida = service.calcular(2,5, '%');
+      expect(invalida).toEqual(0);
+    })
+  );
 });
