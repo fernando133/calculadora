4230b80cda331d30409e342bda6bb4953fa08556
Fernando Martins <fernando@MacBook-Pro-de-Fernando.local>
2018-01-04 10:17:58 -0300

    adicionando servico de opercoes

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,8 +1,47 @@
+/**
+ * 
+ */
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CalculadoraService {
 
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
 
+  /**
+   * Calcula operação matemática de dois números.
+   * Suporta operações: soma, subtração, divisão e multiplicação.
+   *  
+   * @param num1 
+   * @param num2 
+   * @param operacao 
+   */
+  calcular (num1: number, num2: number, operacao: string):number {
+    let resultado: number;
+
+    switch(operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+      break;
+
+      default: 
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
